6f2960fd3dccffff65c729d74b784ddc06c43b3f
Vishal Shukla <snickle1108@gmail.com>
2025-02-21 23:51:23 +0530

    X and Youtube

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from resources.facebook.main import osint as facebook
 from resources.instagram.main import osint as instagram
+from resources.x.main import osint as x
+from resources.youtube.main import osint as youtube
 import asyncio
 
-print("Facebook :",asyncio.run(facebook("zuck")))
-print("Instagram :",asyncio.run(instagram("zuck")))
+#print("Facebook : ",asyncio.run(facebook("zuck")))
+#print("instagram : ",asyncio.run(instagram("zuck")))
+#print("X : ",asyncio.run(x("finkd")))
+print("youtube : ",asyncio.run(youtube("tseries")))
---
+++ resources/youtube/main.py
@@ -0,0 +1,62 @@
+import asyncio
+import random
+from pyppeteer import launch
+from pyppeteer_stealth import stealth
+from bs4 import BeautifulSoup
+
+async def osint(target):
+    url = f'https://www.youtube.com/@{target}'
+
+    try:
+        with open("ua.txt", "r", encoding="utf-8") as file:
+            user_agents = [line.strip() for line in file if line.strip()]
+        if user_agents:
+            user_agent = random.choice(user_agents)
+        else:
+            user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36 Edg/97.0.1072.71"
+    except FileNotFoundError:
+        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36 Edg/97.0.1072.71"
+
+    browser = await launch(
+        headless=True,
+        executablePath=r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe",
+        args=["--no-sandbox", "--disable-setuid-sandbox"]
+    )
+
+    page = await browser.newPage()
+
+    await stealth(page)
+
+    await page.setUserAgent(user_agent)
+
+    await page.evaluateOnNewDocument(
+        "() => { Object.defineProperty(navigator, 'webdriver', { get: () => undefined }) }"
+    )
+
+    await page.goto(url, {"waitUntil": "domcontentloaded"})
+
+    await asyncio.sleep(5)
+
+    content = await page.content()
+    await browser.close()
+
+    soup = BeautifulSoup(content, 'html.parser')
+
+    title = soup.title.string if soup.title else "N/A"
+    body = soup.body.get_text(separator="\n", strip=True) if soup.body else "N/A"
+
+    #print(f"Title:\n{title}\n")
+    # print(f"Body:\n{body}\n")
+
+    try:
+        with open("resources/youtube/tags.txt", "r", encoding="utf-8") as file:
+            titles = {line.strip() for line in file}
+
+        if title.strip() in titles:
+            return 404
+        else:
+            return 200
+
+    except Exception as e:
+        print(f"Error : {e}")
+
