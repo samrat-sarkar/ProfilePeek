a8c96848a08b3325189f88e22c313331251c29fa
SAMRAT SARKAR <samratsarkar9999@gmail.com>
2025-02-25 20:28:50 +0530

    +1

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
 import random
+import shutil
 import string
 import asyncio
 from tqdm.asyncio import tqdm
@@ -6,7 +8,20 @@ from resources.facebook.main import osint as facebook
 from resources.instagram.main import osint as instagram
 from resources.x.main import osint as x
 from resources.youtube.main import osint as youtube
+from resources.snapchat.main import osint as snapchat
+from resources.reddit.main import osint as reddit
 
+def delete_pycache_():
+    resources_path = "resources"
+    if not os.path.exists(resources_path):
+        return
+    for root, dirs, files in os.walk(resources_path):
+        if "__pycache__" in dirs:
+            pycache_path = os.path.join(root, "__pycache__")
+            try:
+                shutil.rmtree(pycache_path)
+            except Exception as e:
+                return
 
 def generate_token():
     return ''.join(random.choices(string.ascii_letters + string.digits, k=36))
@@ -24,12 +39,14 @@ async def check_modules():
         "Facebook": facebook,
         "X": x,
         "YouTube": youtube,
+        "Snapchat": snapchat,
+        "Reddit": reddit,
         "Instagram": instagram
     }
 
     with tqdm(total=len(tasks), desc="Checking Modules", unit="module") as progress:
         coroutines = [check_module(name, func, token, progress) for name, func in tasks.items()]
-        results = await asyncio.gather(*coroutines)
+        results = await asyncio.gather(*coroutines, return_exceptions=False)
 
     for module, status in results:
         if status == 200:
@@ -37,9 +54,13 @@ async def check_modules():
         elif status == 404:
             print(f"{module} Module Up-to-date")
 
-#asyncio.run(check_modules())# Call This Function When You Want To Check That Module is Working Properly Or Not
+asyncio.run(check_modules())# Call This Function When You Want To Check That Module is Working Properly Or Not
 
 # print(asyncio.run(facebook("zuck")))
 # print(asyncio.run(instagram("zuck")))
 #print(asyncio.run(x("zuck")))
 #print(asyncio.run(youtube("zuck")))
+#print(asyncio.run(snapchat("zuck")))
+#print(asyncio.run(reddit("zuck")))
+
+delete_pycache_()
